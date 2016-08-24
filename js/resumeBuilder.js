47dd9aac6f7a15f069d42bceddcfc0a2cbbb7fe0
Sam Frances <sam@samfrances.uk>
2016-08-24 23:59:36 +0100

    Add name and role

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,1 +1,5 @@
-$("#main").append('Sam Frances')
+var name = 'Sam Frances';
+var role = 'Web Developer';
+var formattedName = HTMLheaderName.replace("%data%", name);
+var formattedRole = HTMLheaderRole.replace("%data%", role);
+$("#header").prepend(formattedRole).prepend(formattedName);
