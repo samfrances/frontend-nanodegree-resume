cba8ffe58a6e65c6df5d4539fea2f8718ed07120
Sam Frances <sam@samfrances.uk>
2016-08-25 15:57:30 +0100

    Complete bio exercise

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -3,3 +3,22 @@ var role = 'Web Developer';
 var formattedName = HTMLheaderName.replace("%data%", name);
 var formattedRole = HTMLheaderRole.replace("%data%", role);
 $("#header").prepend(formattedRole).prepend(formattedName);
+
+var bio = {
+	name: 'Sam Frances',
+	role: "Web Developer",
+	contactInfo: 'sam@samfrances.uk',
+	pictureURL: 'images/fry.jpg',
+	welcomeMessage: 'Welcome!',
+	skills: ['Python', 'Javascript'],
+}
+
+var bioFormatted = Object.keys(bio).map(function(key) {
+	if (key === 'pictureURL') {
+		return '';
+	}
+	return "<li>" + key + ": " + bio[key] + "</li>";
+}).join('');
+
+$('#main').append('<img src="' + bio.pictureURL +'" alt="Me">')
+		  .append("<ul>" + bioFormatted + "</ul>");
