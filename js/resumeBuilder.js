b933a9f373f85905735a71f22e1eff5459809daa
Sam Frances <sam@samfrances.uk>
2016-08-26 19:35:20 +0100

    Add code for filling work experience section

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,89 +1,113 @@
+var LIPSUM = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec " +
+            "nec erat efficitur, fermentum risus sit amet, consequat orci. " +
+            "Sed quis condimentum leo. Pellentesque ac vestibulum ante. Nunc " +
+            "cursus tincidunt nunc, hendrerit semper justo consectetur nec. " +
+            "Proin efficitur mi eu justo porttitor scelerisque. Sed " +
+            "tincidunt tempor sem, et vulputate orci imperdiet sit amet. " +
+            "Nullam venenatis faucibus sem vitae posuere.";
+
 var work = {
-	"jobs": [
-		{
-			"employer": "Company A",
-			"title": "Developer",
-			"location": "",
-			"dates": "",
-			"description": ""
-		},
-		{
-			"employer": "Company B",
-			"title": "Programmer",
-			"location": "",
-			"dates": "",
-			"description": ""
-		}
-	]
+    "jobs": [
+        {
+            "employer": "Wonka Industries",
+            "title": "Full-Stack Developer",
+            "location": "Munich",
+            "dates": "2010-2011",
+            "description": LIPSUM
+        },
+        {
+            "employer": "E-Corp",
+            "title": "Web Developer",
+            "location": "New York",
+            "dates": "2011-2014",
+            "description": LIPSUM
+        }
+    ]
 }
 
 var projects = {
-	"projects": [
-		{
-			"title": "",
-			"dates": "",
-			"description": "",
-			"images": []
-		},
-		{
-			"title": "",
-			"dates": "",
-			"description": "",
-			"images": []
-		}
-	]
+    "projects": [
+        {
+            "title": "",
+            "dates": "",
+            "description": "",
+            "images": []
+        },
+        {
+            "title": "",
+            "dates": "",
+            "description": "",
+            "images": []
+        }
+    ]
 }
 
 
 
 var bio = {
-	"name": "",
-	"role": "",
-	"welcomeMessage": "",
-	"contacts": {
-		"mobile": "",
-		"email": "",
-		"github": "",
-		"twitter": "",
-		"location": ""
-	},
-	"skills": ["Python", "Javascript"],
-	"biopic": "",
+    "name": "",
+    "role": "",
+    "welcomeMessage": "",
+    "contacts": {
+        "mobile": "",
+        "email": "",
+        "github": "",
+        "twitter": "",
+        "location": ""
+    },
+    "skills": ["Python", "Javascript"],
+    "biopic": "",
 }
 
 var education = {
-	"schools": [
-		{
-			"name": "",
-			"location": "",
-			"degree": "",
-			"majors": [],
-			"dates": "",
-			"url": ""
-		},
-		{
-			"name": "",
-			"location": "",
-			"degree": "",
-			"majors": [],
-			"dates": "",
-			"url": ""
-		}
-	]
+    "schools": [
+        {
+            "name": "",
+            "location": "",
+            "degree": "",
+            "majors": [],
+            "dates": "",
+            "url": ""
+        },
+        {
+            "name": "",
+            "location": "",
+            "degree": "",
+            "majors": [],
+            "dates": "",
+            "url": ""
+        }
+    ]
 }
 
 if ( 'skills' in bio && bio.skills.length > 0 ) {
-	$( '#header' ).append( HTMLskillsStart );
-	var $skills = $( '#skills' );
-	bio[ 'skills' ].forEach(function( skill ) {
-		$skills.append( HTMLskills.replace( '%data%', skill ) )
-	});
+    $( '#header' ).append( HTMLskillsStart );
+    var $skills = $( '#skills' );
+    bio[ 'skills' ].forEach(function( skill ) {
+        $skills.append( HTMLskills.replace( '%data%', skill ) )
+    });
 }
 
-for ( var i in work.jobs ) {
-	var job = work.jobs[i];
-	$( '#workExperience' ).append( HTMLworkStart );
-	var employer = HTMLworkEmployer.replace( '%data%', job.employer );
-	var title = HTMLworkTitle.replace( '%data%', job.title );
-	$( '.work-entry:last' ).append( employer + title );
-}
+work.jobs.forEach(function( job ) {
+
+    // Create work entry
+    $( '#workExperience' ).append( HTMLworkStart );
+
+    // Cache work entry
+    var $workentry = $( '.work-entry:last' );
+
+    // Create title line
+    var employer = HTMLworkEmployer.replace( '%data%', job.employer );
+    var title = HTMLworkTitle.replace( '%data%', job.title );
+
+    $workentry.append( employer + title );
+
+    // Add  work dates
+    $workentry.append( HTMLworkDates.replace( "%data%", job.dates ) );
+
+    // Add location
+    $workentry.append( HTMLworkLocation.replace( "%data%", job.location ) );
+
+    // Add description
+    $workentry.append( HTMLworkDescription.replace( "%data%", job.description) );
+});
